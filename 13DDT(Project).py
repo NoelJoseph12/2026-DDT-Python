224b18c806a013f9943b8fc2e1b38fa352a8889a
NoelJoseph12 <pro.noob.12121212@gmail.com>
2026-07-21 10:30:17 +1200

    test 21072026

--- 13DDT(Project).py
+++ 13DDT(Project).py
@@ -1,45 +1,18 @@
-import tkinter as tk
+import tkinter as tk #Imports the tkinter library into my program and uses a short form of 'tk' for ease.
 
-from tkinter import *
+from tkinter import * #From the tkinter library import *(Everything).
 
-root = tk.Tk()
+root = tk.Tk() #Creates a main application window. Tk() is the window that contains all the buttons, text boxes etc.
+root.geometry("500x600") #Sets the size of the window to 500 pixels wide and 600 pixels tall.
+root.title("Khaos - Workout Tracker") #Creates a title for the window application called "Khaos - Workout Tracker".
 
-root.geometry("500x500")
-root.title("My First GUI")
+root.configure(bg="black") #Changes the background color of the window application to black.
 
-label = tk.Label(root, text="Hello World!", font=('Arial', 18))
-label.pack(padx=20, pady=20)
+title_label = tk.Label(root, text="Khaos", font=('Arial', 28, 'bold'), bg="black", fg="white") #Creates a title label called 'Khaos', while allowing me to edit it to have preferred font, size, colour and weight.
+title_label.pack(padx=20, pady=20) #Creates a 20 pixel padding horizontally and vertically around the title label.
 
-textbox = tk.Text(root, height=3, font=('Arial', 16))
-textbox.pack(padx=10)
+subtitle_label = tk.Label(root, text="Measure The Mayhem", font=('Arial', 22, 'bold'), bg="black", fg="white") #Creates a subtitle label called 'Measure The Mayhem', while allowing me to edit it to have preferred font, size, colour and weight.
+subtitle_label.pack(padx=10, pady=10) #Creates a 10 pixel padding horizontally and vertically around the subtitle label.
 
-#button = tk.Button(root, text="Click Me!", font=('Arial, 18'))
-#button.pack(padx=10, pady=10)
-
-buttonframe = tk.Frame(root)
-buttonframe.columnconfigure(0, weight=1)
-buttonframe.columnconfigure(1, weight=1)
-buttonframe.columnconfigure(2, weight=1)
-
-btn1 = tk.Button(buttonframe, text="1", font=('Arial', 18))
-btn1.grid(row=0, column=0, sticky=tk.W+tk.E)
-
-btn2 = tk.Button(buttonframe, text="2", font=('Arial', 18))
-btn2.grid(row=0, column=1, sticky=tk.W+tk.E)
-
-btn3 = tk.Button(buttonframe, text="3", font=('Arial', 18))
-btn3.grid(row=0, column=2, sticky=tk.W+tk.E)
-
-btn4 = tk.Button(buttonframe, text="4", font=('Arial', 18))
-btn4.grid(row=1, column=0, sticky=tk.W+tk.E)
-
-buttonframe.pack(fill='x')
-
-anotherbtn = tk.Button(root, text="TEST")
-anotherbtn.place(x=200, y=200, height=100, width=100)
-
-
-#myentry = tk.Entry(root)
-#myentry.pack()
-
-root.mainloop()
+root.mainloop() #Starts the tkinter's event loop, which keeps the window open while waiting for user interaction.
+change 1
